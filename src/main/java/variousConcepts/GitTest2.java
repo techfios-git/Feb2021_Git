a39a561bbadfc34b8af46a17d28ab2173e13483d
shihabamin <amin.khandaker@gmail.com>
2021-07-10 13:45:05 -0500

    4th commit

--- src/main/java/variousConcepts/GitTest2.java
+++ src/main/java/variousConcepts/GitTest2.java
@@ -5,7 +5,13 @@ public class GitTest2 {
 	public static void main(String[] args) {
 		System.out.println("Git test");
 		System.out.println("Git test1");
+		printMe();
+	}
+	
+	public static void printMe() {
 		System.out.println("Git test2");
+		System.out.println("Git test3");
+		System.out.println("Git test4");
 	}
 
 }
